748e49d560a1567f14b4ae0c157d0057a207cc08
yujidxl <1435409156@qq.com>
2019-07-09 22:55:09 +0800

    添加路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,20 @@
 import RouteConfig from './router.config';
 import React from 'react';
-import {HashRouter as Router, Route} from 'react-router-dom';
+import  Home  from '../pages/home/app'
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 
 
 export default function(){
   return (
     <Router>
-      {/* <Route path ={RouteConfig[1].path} component={RouteConfig[1].component}></Route> */}
-      {
-        RouteConfig.map( (val, key) =>(
-          <Route path={val.path} key={key} component={val.component} exact={val.path === '/'? true: false}></Route>
-        ))
-      }
+      <Switch>
+        {
+          RouteConfig.map( (val, key) =>(
+            <Route path={val.path} key={key} component={val.component} exact={val.path === '/'? true: false}></Route>
+          ))
+        }
+        <Route component={Home}></Route>
+      </Switch>
     </Router>
   )
 }
