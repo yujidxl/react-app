aaa5af68f90035077c0b1e229025900e115fec29
yujidxl <1435409156@qq.com>
2019-07-10 20:04:46 +0800

    增添redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
 import './index.scss';
+import './Count'
 // import App from './pages/home/app';
-import HomeRoute from  './router/index'
+// import HomeRoute from  './router/index'
 // import BasicExample from './BsRouter';
+import App from './components/App'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<HomeRoute />, document.getElementById('root'));
+
+const store = createStore(rootReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
